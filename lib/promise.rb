669f00e8bc3f04c386e928ce1d2bf3c8a51c8e33
Arthur Schreiber <arthurschreiber@github.com>
2018-12-18 20:43:27 +0100

    Fix promise rejection.

--- lib/promise.rb
+++ lib/promise.rb
@@ -225,7 +225,7 @@ class Promise
 
   def notify_rejection
     @observers.each_slice(3) do |observer, _on_fulfill_arg, on_reject_arg|
-      QUEUE.enqueue_microtask(observer, :promise_rejected, [value, on_reject_arg])
+      QUEUE.enqueue_microtask(observer, :promise_rejected, [reason, on_reject_arg])
     end
 
     @observers = nil
